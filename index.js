8759c3cd98f70d26de517adf5f62f1a43278620d
Firooz Mafakaheri <firooz.mafakheri@gmail.com>
2019-08-21 21:48:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function theBeatlesPlay(musician,instrument){
   var arry=[];
   for(var i=0;i<musician.length;i++){
-    arry.push(musician[i]+"play"+instrument[i]);
+    arry.push(musician[i]+" play "+instrument[i]);
   }
   return arry;
 }
